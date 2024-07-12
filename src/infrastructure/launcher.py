5575e1e17307f26b7414726e29ffcc888d3680d7
Sarah Burke Spolaor <sarahbspolaor@gmail.com>
2024-07-12 19:09:10 -0400

    Made launcher consistent with new db query structure.

--- src/infrastructure/launcher.py
+++ src/infrastructure/launcher.py
@@ -33,12 +33,13 @@ THERE ARE SEVERAL REASONS LAUNCHER SHOULD FORCE-FAIL:
 # -----------------------------------------------
 # General Module Imports
 # -----------------------------------------------
-import requests   # Allow communications with TPP-DB API.
+import requests   # Allow communications with TPP-DB API.#!!!! Superceded by db functionality
 import subprocess # To call sbatch/slurm.
 import yaml       # For reading private authentication data.
 import globus_sdk as globus
 from globus_sdk.scopes import TransferScopes
 import argparse
+import database as db
 
 def manage_single_transfer(transfer_client, src, dest, src_location, dest_location):
     # Initiate Transfer using TransferClient
@@ -62,40 +63,37 @@ def manage_single_transfer(transfer_client, src, dest, src_location, dest_locati
 # -----------------------------------------------
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Launches the TPP pipeline.")
-    parser.add_argument('--data_id', '-d', dest='data_id', type=int, default=None,
+    parser.add_argument('--dataID', '-d', dest='dataID', type=int, default=None,
                         help="The Unique Data Identifier for the file to be processed by the pipeline.")
-    parser.add_argument('--config', '-c', dest='config_file', type=str, default=None,
-                        help="The user-specific Configuration File required for the pipeline.")
     args= parser.parse_args()
 
     # -----------------------------------------------
     # Data Identifier & Configuration File
     # -----------------------------------------------
     # Data ID requested by user (unique identifier in DM of file to be processed).
-    data_id = args.data_id
-    if data_id == None:
-        data_id = input("Please enter the Unique Data Identifier to be processed: ").strip()
+    dataID = args.dataID
+    if dataID == None:
+        print("Please enter the Unique Data Identifier to be processed. Otherwise this code cannot run.")
+        exit()
 
-    # Configuration YAML provided by the user (contains tokens, networking settings, etc)
-    config_file = args.config_file
-    if config_file == None:
-        config_file = input("Please enter the absolute path of your TPP Configuration File: ").strip()
+    
 
-    # Read config file for authentication info
-    with open(config_file, 'r') as file:
-        config = yaml.safe_load(file)
+   
+    # -----------------------------------------------
+    # Check that the latest pipeline is what's being run.
+    # -----------------------------------------------
 
-    # Set Required Variables
-    tppdb_ip = config['tpp-db']['url']
-    tppdb_port = config['tpp-db']['port']
-    user_token = config['tpp-db']['token']
-
+    try:
+        current_pipelineID = db.current_pipelineID()
+    except:
+        exit()
+        
     # -----------------------------------------------
     # GLOBUS Configuration
     # -----------------------------------------------
 
     # Requires Globus Authentication with West Virginia University as the IdP
-    CLIENT_ID = config['globus']['client_id']
+    CLIENT_ID = db.dbconfig.globus_client_id
     auth_client = globus.NativeAppAuthClient(CLIENT_ID)
     auth_client.oauth2_start_flow(refresh_tokens=True, requested_scopes=TransferScopes.all)
 
@@ -111,30 +109,34 @@ if __name__ == "__main__":
     tc = globus.TransferClient(authorizer=globus.AccessTokenAuthorizer(transfer_tokens["access_token"]))
 
     # Set Up the Storage Collection and Compute Collection IDs
-    storage = config['globus']['storage_collection_id']
-    compute = config['globus']['compute_collection_id']
-
-    # -----------------------------------------------
-    # TPP-Database Communication Configuration
-    # -----------------------------------------------
-    tppdb_base = "http://" + tppdb_ip + ":" + tppdb_port
-    tppdb_data = tppdb_base + "/data"
-    headers = {"Authorization": f"Bearer{user_token}"}
-    #TPPDB Check connection and auth info here.
+    storage = db.dbconfig.globus_stor_id
+    compute = db.dbconfig.globus_comp_id
     
     # -----------------------------------------------
     # Transfer Necessary Files to Compute FS
     # -----------------------------------------------
-    # Get the location of DATA_ID on Storage from TPP-DB
+    # Get the location of dataID on Storage from TPP-DB
     ## TODO: Sarah needs to add a comms failure check at this point.
-    db_request = requests.get(tppdb_data + "/" + data_id, headers=headers).json()
-    stor_location = db_request['location_on_filesystem']
-    #TPPDB INITIATE OUTCOMES, SUBMISSIONS, and RESULTS instances here, after we are sure that the job is going to be launched. This way, the TPP pipeline only has to deal with updating information (not creating a new document) and knows the submissionID, dataID, etc relevant to this job.
-    # Want to have a command that initiates the documents in these three collections.
+    # Test DB connection and existence of data ID.
+    try:
+        db_response = db.get("data",dataID)
+    except:
+        # Hopefully db will print all appropriate errors.
+        # Here we want to exit if there are fundamental issues with the DB.
+        exit()
+
+    stor_location = db_response['location_on_filesystem']
+
+    #TPPDB INITIATE OUTCOMES and SUBMISSIONS documents here (and RESULTS?), after we are sure that the job is going to be launched. This way, the TPP pipeline only has to deal with updating information (not creating a new document) and knows the submissionID, dataID, etc relevant to this job.
+    try:
+        submissionID = db.init_document("job_submissions",dataID,pipelineID=current_pipelineID)
+        outcomeID = db.init_document("processing_outcomes",dataID,submissionID=submissionID)
+    except:
+        exit()
 
     # Construct the Location on Compute FS
     ## TODO: Finalize FS Structure on Compute
-    comp_location = config['globus']['compute_scratch_dir']+"BLAHBLAHBLAH"
+    comp_location = db.dbconfig.globus_scratch_dir+"BLAHBLAHBLAH"
 
     # Transfer the Required files from Storage to Compute
     manage_single_transfer(tc, storage, compute, stor_location, comp_location)
@@ -143,6 +145,7 @@ if __name__ == "__main__":
     # Launch the TPP Pipeline via the SLURM Command
     # -----------------------------------------------
 
+    #!H!H!H
     # Use command line to call slurm.
     #subprocess.run(["sbatch","--time=5-23:45:00 --nodes=1 --ntasks-per-node=10 --job-name=\"Reshma\" --partition=thepartitiontouse --wrap=\"SINGULARITY CALL ; COMMAND TO RUN"]) ###### NEED TO FIX THIS
 
@@ -156,13 +159,39 @@ if __name__ == "__main__":
     # Transfer Products from Compute to Storage
     # -----------------------------------------------
     #Construct the Location on Compute FS of Products
-    comp_location = config['globus']['compute_scratch_dir']+"/BLAHBLAHBLAH"+".hdf5"
+    comp_location_hdf5 = db.dbconfig.globus_comp_dir+"BLAHBLAHBLAH"+".hdf5"
 
     #Construct the Location on Storage FS of Products
-    stor_location = config['globus']['storage_result_dir']+"/BLAHBLAHBLAH"+".hdf5"
+    stor_location_hdf5 = db.dbconfig.globus_res_dir+"/BLAHBLAHBLAH"+".hdf5"
 
     #Transfer the Final Products from Compute to Storage
-    manage_single_transfer(tc, compute, storage, comp_location, stor_location)
+    manage_single_transfer(tc, compute, storage, comp_location_hdf5, stor_location_hdf5)
 
 
     
+# CUSTOM CONFIG FILE FUNCTIONALITY not yet allowed or implemented. It remains here as a historical relic.
+#config    parser.add_argument('--config', '-c', dest='config_file', type=str, default=None,
+#config                        help="The user-specific Configuration File required for the pipeline (USUALLY YOU SHOUDL NOT SPECIFY THIS. By default user's config.yml will be read from the TPP pipeline install directory.", required=False)
+#config    # Configuration YAML provided by the user (contains tokens, networking settings, etc)
+#config    config_file = args.config_file
+#config    if config_file == None:
+#config        config_file = input("Please enter the absolute path of your TPP Configuration File: ").strip()
+#config
+#config    # Read config file for authentication info
+#config    with open(config_file, 'r') as file:
+#config        config = yaml.safe_load(file)
+#config
+#config    # Set Required Variables
+#config    tppdb_ip = config['tpp-db']['url']
+#config    tppdb_port = config['tpp-db']['port']
+#config    user_token = config['tpp-db']['token']
+#config    # -----------------------------------------------
+#config    # TPP-Database Communication Configuration
+#config    # -----------------------------------------------
+#config    tppdb_base = "http://" + tppdb_ip + ":" + tppdb_port
+#config    tppdb_data = tppdb_base + "/data"
+#config    headers = {"Authorization": f"Bearer{user_token}"}
+#config    CLIENT_ID = config['globus']['client_id']
+#config    storage = config['globus']['storage_collection_id']
+#config    compute = config['globus']['compute_collection_id']
+#config    comp_location = config['globus']['compute_scratch_dir']+"BLAHBLAHBLAH"
